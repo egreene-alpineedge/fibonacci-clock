c3d8cfb82c4fb19b69c3914ff7daefd05f00e98a
egreene-alpineedge <egreene@alpineedge.io>
2026-09-12 21:55:13 -0400

    fixed logical reasoning

--- arduino/sketch_sep12a/sketch_sep12a.ino
+++ arduino/sketch_sep12a/sketch_sep12a.ino
@@ -1,6 +1,9 @@
 
-long SECONDS_IN_HOUR = 60*60;
-long TOTAL_STEPS = 2048;        // Total steps in step motor
+long SECONDS_IN_HOUR = 60L*60;
+long TOTAL_MINUTE_HAND_STEPS = 2048L;        // Total steps in step motor
+
+long SECONDS_IN_12_HOUR = 60L*60*12;       // 12 hours is one rotation
+long TOTAL_HOUR_HAND_STEPS = 2048L;        // Total steps in step motor
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -10,18 +13,20 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-
-  // Hours Hand - Main Spiral
+  // Get current time
   long time_in_seconds = getCurrentTime();
-  long step_sec = ((double)TOTAL_STEPS / (double)SECONDS_IN_HOUR) * time_in_seconds;
-  long step = step_sec % TOTAL_STEPS;
-  Serial.println(step);
 
+  // Minute Hand - Short Hand
+  long hour_step = (TOTAL_MINUTE_HAND_STEPS * (time_in_seconds % SECONDS_IN_HOUR)) / SECONDS_IN_HOUR;
+  // Serial.println(hour_step);
 
-  // 
+  // Hour Hand - Spiral Hand
+  long twelve_hour_step = (TOTAL_HOUR_HAND_STEPS * (time_in_seconds % SECONDS_IN_12_HOUR)) / SECONDS_IN_12_HOUR;
+  Serial.println(twelve_hour_step);
+
 }
 
 long getCurrentTime() {
-  return 3650;
+  return 3600;
 }
 
